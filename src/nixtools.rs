3784222cbb0adf0bf916655a3d6af9703deccd58
Ivan Velichko <iximiuz@gmail.com>
2019-11-16 23:33:34 +0100

    wip

--- src/nixtools.rs
+++ src/nixtools.rs
@@ -1,19 +1,86 @@
 use libc::{self, c_int, c_ulong};
 use nix::errno::Errno;
+use nix::fcntl::{open, OFlag};
 use nix::sys::signal::{sigprocmask, SigSet, SigmaskHow, Signal};
-use nix::unistd::{self};
+use nix::sys::stat::Mode;
+use nix::unistd::{close, dup2, pipe2, setsid};
 use nix::Result;
+use std::os::unix::io::RawFd;
+
+pub fn null_stdio_streams() {
+    let dev_null_r = open(
+        "/dev/null",
+        OFlag::O_RDONLY | OFlag::O_CLOEXEC,
+        Mode::empty(),
+    )
+    .expect("open('/dev/null') for reading failed");
+
+    let dev_null_w = open(
+        "/dev/null",
+        OFlag::O_WRONLY | OFlag::O_CLOEXEC,
+        Mode::empty(),
+    )
+    .expect("open('/dev/null') for writing failed");
+
+    set_stdio_streams(STDIO {
+        IN: Some(dev_null_r),
+        OUT: Some(dev_null_w),
+        ERR: Some(dev_null_w),
+    });
+
+    close(dev_null_r).expect("close('/dev/null (RD)') failed");
+    close(dev_null_w).expect("close('/dev/null (WR)') failed");
+}
+
+pub struct Pipe {
+    rd: RawFd,
+    wr: RawFd,
+}
+
+impl Pipe {
+    pub fn rd(&self) -> RawFd {
+        self.rd
+    }
+    pub fn wr(&self) -> RawFd {
+        self.wr
+    }
+}
+
+pub fn create_pipe() -> Pipe {
+    let (rd, wr) = pipe2(OFlag::O_CLOEXEC).expect("pipe2() failed");
+    Pipe{ rd, wr }
+}
+
+pub fn session_start() {
+    setsid().expect("sessid() failed");
+}
 
 pub fn set_child_subreaper() {
-    prctl(PrctlOption::PR_SET_CHILD_SUBREAPER, 1, 0, 0, 0).expect("oops");
+    prctl(PrctlOption::PR_SET_CHILD_SUBREAPER, 1, 0, 0, 0)
+        .expect("prctl(PR_SET_CHILD_SUBREAPER) failed");
 }
 
 pub fn set_parent_death_signal(sig: Signal) {
-    prctl(PrctlOption::PR_SET_PDEATHSIG, sig as c_ulong, 0, 0, 0).expect("oops");
+    prctl(PrctlOption::PR_SET_PDEATHSIG, sig as c_ulong, 0, 0, 0)
+        .expect("prctl(PR_SET_PDEATHSIG) failed");
 }
 
-pub fn setsid() {
-    unistd::setsid().expect("must always succeed, see man 2 sessid");
+pub struct STDIO {
+    pub IN: Option<RawFd>,
+    pub OUT: Option<RawFd>,
+    pub ERR: Option<RawFd>,
+}
+
+pub fn set_stdio_streams(streams: STDIO) {
+    if let Some(fd) = streams.IN {
+        dup2(fd, 0).expect("dup2(fd, STDIN_FILENO) failed");
+    }
+    if let Some(fd) = streams.OUT {
+        dup2(fd, 1).expect("dup2(fd, STDOUT_FILENO) failed");
+    }
+    if let Some(fd) = streams.ERR {
+        dup2(fd, 2).expect("dup2(fd, STDERR_FILENO) failed");
+    }
 }
 
 pub fn signals_block(signals: &[Signal]) -> SigSet {
@@ -22,12 +89,14 @@ pub fn signals_block(signals: &[Signal]) -> SigSet {
         newmask.add(*s);
     }
     let mut oldmask = SigSet::empty();
-    sigprocmask(SigmaskHow::SIG_BLOCK, Some(&newmask), Some(&mut oldmask)).expect("ooops");
+    sigprocmask(SigmaskHow::SIG_BLOCK, Some(&newmask), Some(&mut oldmask))
+        .expect("sigprocmask(SIG_BLOCK) failed");
     return oldmask;
 }
 
 pub fn signals_restore(mask: &SigSet) {
-    sigprocmask(SigmaskHow::SIG_SETMASK, Some(&mask), None).expect("ooops");
+    sigprocmask(SigmaskHow::SIG_SETMASK, Some(&mask), None)
+        .expect("sigprocmask(SIG_SETMASK) failed");
 }
 
 #[repr(i32)]
--- src/shimmy.rs
+++ src/shimmy.rs
@@ -3,51 +3,79 @@ use std::path::Path;
 use std::process::exit;
 
 use libc::_exit;
-use nix::fcntl::{OFlag, open};
 use nix::sys::signal::Signal::{SIGINT, SIGKILL, SIGQUIT, SIGTERM};
-use nix::sys::stat::Mode;
-use nix::unistd::{ForkResult, Pid, dup2, fork};
+use nix::unistd::{fork, ForkResult, Pid};
 
 mod nixtools;
 use nixtools::{
-    set_child_subreaper, set_parent_death_signal, setsid, signals_block, signals_restore,
+    create_pipe, null_stdio_streams, session_start, set_child_subreaper, set_parent_death_signal,
+    set_stdio_streams, signals_block, signals_restore, Pipe, STDIO,
 };
 
 fn main() {
-    // parse args
+    // Main process
+    // TODO: parse args
 
     match fork() {
-        Err(err) => {
-            panic!("fork() of the shim process failed: {}", err);
-        }
         Ok(ForkResult::Parent { child, .. }) => {
+            // Main process
             write_pid_file_and_exit("/home/vagrant/shimmy/pidfile.pid", child);
         }
         Ok(ForkResult::Child) => {
-            null_std_streams();
-            setsid();
+            // Shim process
+            null_stdio_streams();
+            session_start();
             set_child_subreaper();
-            // make pipes for runc stdout/stderr
+
+            let pipes = create_runtime_stdio_pipes();
             let oldmask = signals_block(&[SIGINT, SIGQUIT, SIGTERM]);
 
             match fork() {
-                Err(err) => {
-                    panic!("fork() of the container process failed: {}", err);
-                }
-                Ok(ForkResult::Parent { child, .. }) => {
-                    // run server
-                    //   read from stdout/stderr & dump to log
-                    //   dump exit code on runc exit
-                }
                 Ok(ForkResult::Child) => {
-                    set_parent_death_signal(SIGKILL); // TODO: check does it still work after exec)
+                    // Container runtime process
+                    // TODO: check does it still work after exec)
+                    set_parent_death_signal(SIGKILL);
                     signals_restore(&oldmask);
-                    // dup std streams
-                    exec_oci_runtime_or_exit();
+                    set_stdio_streams(STDIO {
+                        IN: Some(pipes.stdin.rd()),
+                        OUT: Some(pipes.stdout.wr()),
+                        ERR: Some(pipes.stderr.wr()),
+                    });
+                    exec_oci_runtime_or_die();
+                }
+                Ok(ForkResult::Parent { .. }) => {
+                    // Shim process
+                    start_shim_server(pipes);
+                }
+                Err(err) => {
+                    panic!("fork() of the container runtime process failed: {}", err);
                 }
             }
         }
+        Err(err) => {
+            panic!("fork() of the shim process failed: {}", err);
+        }
     };
+}
+
+struct Pipes {
+    stdin: Pipe,
+    stdout: Pipe,
+    stderr: Pipe,
+}
+
+fn create_runtime_stdio_pipes() -> Pipes {
+    Pipes {
+        stdin: create_pipe(),
+        stdout: create_pipe(),
+        stderr: create_pipe(),
+    }
+}
+
+fn start_shim_server(_runtime_stdio: Pipes) {
+    // run server
+    //   read from stdout/stderr & dump to log
+    //   dump exit code on runc exit
 }
 
 fn write_pid_file_and_exit<P: AsRef<Path>>(filename: P, pid: Pid) {
@@ -61,17 +89,8 @@ fn write_pid_file_and_exit<P: AsRef<Path>>(filename: P, pid: Pid) {
     exit(0);
 }
 
-fn exec_oci_runtime_or_exit() {
+fn exec_oci_runtime_or_die() {
     unsafe {
         _exit(127);
     }
-}
-
-fn null_std_streams() {
-    let dev_null_r = open("/dev/null", OFlag::O_RDONLY, Mode::empty()).expect("oops");
-    let dev_null_w = open("/dev/null", OFlag::O_WRONLY, Mode::empty()).expect("oops");
-
-    dup2(dev_null_r, 0).expect("oops");
-    dup2(dev_null_w, 1).expect("oops");
-    dup2(dev_null_w, 2).expect("oops");
 }
